6b34195f5b5ee55301c6382b6c0d970a788f46f4
wavesforweirdos <wavesforweirdos@gmail.com>
2023-01-11 20:18:48 +0100

    Tailwind Vue & Vite configured

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./resources/**/*.{vue,js,ts,jsx,tsx}",
+  ],  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
