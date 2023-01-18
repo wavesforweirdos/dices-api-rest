4b8ccffd8a2a550a85874aaf87f0b11bc8a24d1d
wavesforweirdos <wavesforweirdos@gmail.com>
2023-01-19 00:59:22 +0100

    correct api-routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\API\AuthUserController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\GameController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,16 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/players', [AuthUserController::class, 'register'])->name('register'); //create player
-Route::post('/login', [AuthUserController::class, 'login'])->name('login');
+Route::post('/players', [AuthController::class, 'register'])->name('register'); //create player
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('/logout', [AuthUserController::class, 'logout'])->name('logout');
-    Route::put('/players/{id}', [AuthUserController::class, 'edit'])->name('editName'); //edit name's player 
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::put('/players/{id}', [UserController::class, 'edit'])->name('editName'); //edit name's player 
 
     Route::post('/players/{id}/games/', [GameController::class, 'throw'])->name('throw'); //a player throw dices
-    Route::delete('/players/{id}/games', [GameController::class, 'destroyThrowsFromUser'])->name('deleteThrows'); //delete all player's throws
-    Route::get('/players/{id}/games', [GameController::class, 'showAllGamesFromUser'])->name('throws'); //throws of onePlayer
+    Route::delete('/players/{id}/games', [UserController::class, 'destroyThrowsFromUser'])->name('deleteThrows'); //delete all player's throws
+    Route::get('/players/{id}/games', [UserController::class, 'showAllGamesFromUser'])->name('throws'); //throws of onePlayer
 
     Route::get('/players', [GameController::class, 'fullSuccessRateRecord'])->name('admin.fullSuccessRateRecord'); //allPlayers & their %victories
     Route::get('/players/ranking', [GameController::class, 'ranking'])->name('ranking'); //%victories of allGame sortBy Asc
